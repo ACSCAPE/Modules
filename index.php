c2a2e01e1cd81c5c144d340371b20c765cd1b759
JBoulenger21 <boulenger.julie@gmail.com>
2020-10-09 16:28:04 +0200

    installation de bootstrap et mise en place des fichiers css et js

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Test module</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <button type="button" name="Escape Game">Créer un Escape Game</button>
+    <form class="" name ="" action="traitement.php" method="post">
+
+    </form>
+    <form class="" name ="" action="traitement.php" method="post">
+
+    </form>
+    <form class="" name ="" action="traitement.php" method="post">
+
+    </form>
+    <form class="" name ="" action="traitement.php" method="post">
+
+    </form>
+    <script type="text/javascript" src="main.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
+  </body>
+</html>
